ab8372fdd88b7b911e5b8cd4a132604557b859de
carlschader <carlschader@gmail.com>
2021-07-13 12:24:49 -0600

    feat: added jwt validation

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,33 @@
 const express = require('express');
+const { getdb } = require('@plandid/mongo-utils');
+const { ObjectID } = require('mongodb');
+const jwt = require('express-jwt');
+
+const jwtOptions = {
+    secret: process.env.AUTH_SECRET,
+    algorithms: ['RS256']
+};
+
+const col = getdb().collection('records');
 
 const router = express.Router();
+
+router.get('/:accessKey', async function(req, res, next) {
+    try {
+        const data = await col
+        .find({ _id: new ObjectID(req.params.accessKey) })
+        .limit(1)
+        .project({ _id: 0, record: 1 })
+        .next();
+        res.json(data ? data.record : {});
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.use(jwt(jwtOptions), async function(req, res, next) {
+    console.log(req.user);
+});
 
 router.use('/records', require('./routes/records'));
 
--- src/routes/records.js
+++ src/routes/records.js
@@ -1,23 +1,9 @@
 const express = require('express');
 const { getdb } = require('@plandid/mongo-utils');
-const { ObjectID } = require('mongodb');
 
 const col = getdb().collection('records');
 
 const router = express.Router();
-
-router.get('/:_id', async function(req, res, next) {
-    try {
-        const data = await col
-        .find({ _id: new ObjectID(req.params._id) })
-        .limit(1)
-        .project({ _id: 0, record: 1 })
-        .next();
-        res.json(data ? data.record : {});
-    } catch (error) {
-        next(error);
-    }
-});
 
 router.get('/names/:sub/', async function(req, res, next) {
     try {
