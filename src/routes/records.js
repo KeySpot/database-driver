c894d02e75706480f5be80421a503da5c7eeeccc
carlschader <carlschader@gmail.com>
2021-07-13 09:03:01 -0600

    feat: added name and count methods

--- src/routes/records.js
+++ src/routes/records.js
@@ -11,9 +11,34 @@ router.get('/:_id', async function(req, res, next) {
         const data = await col
         .find({ _id: new ObjectID(req.params._id) })
         .limit(1)
-        .project({_id: 0, record: 1})
+        .project({ _id: 0, record: 1 })
         .next();
         res.json(data ? data.record : {});
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get('/names/:sub/', async function(req, res, next) {
+    try {
+        const data = await col
+        .find({ sub: req.params.sub })
+        .project({ _id: 0, name: 1 })
+        .toArray();
+        res.json(data.map(function(element) {
+            return element.name;
+        }));
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get('/count/:sub/', async function(req, res, next) {
+    try {
+        const data = await col
+        .find({ sub: req.params.sub })
+        .count();
+        res.json(data);
     } catch (error) {
         next(error);
     }
